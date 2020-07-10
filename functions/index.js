4f86e325abf5e9e7f15a21348d45271aab35cdf3
Alex <gombobobo@gmail.com>
2020-07-11 00:00:03 +0300

    Set up basic database with requests and shit0

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
 const functions = require('firebase-functions');
+const admin = require("firebase-admin") 
+
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,4 +9,36 @@ const functions = require('firebase-functions');
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello World!");
+});
+
+exports.getSnippets = functions.https.onRequest((req, res) => {
+  admin.firestore().collection("snippets").get()
+    .then(data => {
+      let snippets = []
+      data.forEach(doc => {
+         snippets.push(doc.data());
+      });
+      return res.json(snippets);
+    })
+    .catch(err => console.error(err));
+})
+
+exports.createSnippet = functions.https.onRequest((req, res) => {
+  const newSnippet = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+  };
+
+  admin
+    .firestore()
+    .collection("snippets")
+    .add(newSnippet)
+    .then((doc) => {
+        res.json({ message: `document ${doc.id} created successfully`})
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "Something went wrong"})
+      console.error(err); 
+    });
 });
