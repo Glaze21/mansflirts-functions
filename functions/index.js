33403bc4db4fb87760bfc7728f9e0e741c9563a9
Alex <gombobobo@gmail.com>
2020-07-11 16:58:04 +0300

    updated node to v14

--- functions/index.js
+++ functions/index.js
@@ -3,9 +3,6 @@ const admin = require("firebase-admin")
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello World!");
