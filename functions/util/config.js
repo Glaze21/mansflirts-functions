fa6296dd1aa21e9be441f9bc15dcd6d9424047b6
Alex <gombobobo@gmail.com>
2020-07-14 17:15:39 +0300

    Refactor

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyB8vU6CCJTykyml4QSzjDG2wYgfyiL9_GA",
+  authDomain: "dating-site-e5be3.firebaseapp.com",
+  databaseURL: "https://dating-site-e5be3.firebaseio.com",
+  projectId: "dating-site-e5be3",
+  storageBucket: "dating-site-e5be3.appspot.com",
+  messagingSenderId: "606732221277",
+  appId: "1:606732221277:web:6ac74a09f2fe395f9d86b4",
+  measurementId: "G-F0NLQ4NTQR",
+};
